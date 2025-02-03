e66de0da2972646e5f7e1cc09ce8bd885c7e8c1b
demin-ia <defoxyweex@gmail.com>
2025-02-03 21:38:25 +0300

    Initial commit

---
+++ src/main/kotlin/com/point/envelope/EnvelopeBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.point.envelope
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EnvelopeBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<EnvelopeBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/point/envelope/EnvelopeBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.point.envelope
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EnvelopeBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
